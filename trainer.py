c93f52ba9cae55fd73a9b5835d1deea4c7b016fa
Zihao Zou <zihao@cigserver2.seas.wustl.edu>
2022-03-25 23:06:47 -0500

    disable scheduler warmup

--- trainer.py
+++ trainer.py
@@ -91,7 +91,7 @@ def main(config):
             config.blur.model.warmup, map_location=f'cuda:{config.blur.train.devices[0]}')
         model.module.load_state_dict(warmUpDict['model'])
         optimizer.load_state_dict(warmUpDict['optimizer'])
-        scheduler.load_state_dict(warmUpDict['scheduler'])
+        #scheduler.load_state_dict(warmUpDict['scheduler'])
     logger=SummaryWriter(log_dir='logs')
     mkdir('ckpts')
 
